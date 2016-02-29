2494e1f45e7c45df49a3653cfa77262c65006d65
Jim Dixon <jddixon@gmail.com>
2016-02-29 15:47:51 -0800

    add sentencizer and test

--- nlp/__init__.py
+++ nlp/__init__.py
@@ -1,10 +1,20 @@
 # buildList/__init__.py
 
 
+from nltk.tokenize import sent_tokenize
+
 __all__ = ['__version__', '__version_date__',
+            'sentencize',
           ]
 
-__version__      = '0.0.2'
-__version_date__ = '2016-02-25'
+__version__      = '0.0.3'
+__version_date__ = '2016-02-29'
 
+
+def sentencize(txt):
+    """
+    Given a block of test txt, returns a list of sentences carved
+    out of it.
+    """
+    return sent_tokenize(txt)
 
